030cbd0eee9f0d00265a6705d7d619753c2d4d32
Yuliya Maximchikova <m.yuliyas.00@gmail.com>
2019-09-20 21:56:27 +0300

    03-01

--- 03-01.js
+++ 03-01.js
@@ -1,1 +1,9 @@
-var http = require('http');
+let http = require('http');
+let state = 'norm';
+
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.end('<h1>' + state + '</h1>');
+}).listen(5000);
+
+console.log('Server running at http://localhost:5000/');
